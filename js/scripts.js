de46726bfa06cfbe61a51b5ea75e957f638fed16
almazka <almazka@flylady.su>
2015-05-07 12:21:33 +0600

    modified from slick

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,5 @@
 $(document).ready(function(){
-	if($('.slider-area').length > 0){
-		$('.slider-area').slick({
-			autoplay: false,
-			prevArrow: '.photo-slider .prev',
-			nextArrow: '.photo-slider .next',
-			dots: true,
-			infinite: true,
-			speed: 500,
-			fade: true,
-			cssEase: 'linear'
-		});
-	}
+	
 	/*ancor*/
 	$('a[href*=#]').bind("click", function(e){
 	     var anchor = $(this);
